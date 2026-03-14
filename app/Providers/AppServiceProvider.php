4034839919f6eac37e158cc576670a024b2ddc7e
hourt <bunhourt79@gmail.com>
2026-03-14 11:36:54 +0700

    feat: Configure initial Laravel application setup, including core bootstrapping, service providers, application settings, and Vercel deployment environment variables.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
-        URL::forceHttps();
+        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+            URL::forceHttps();
+        }
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
+        $middleware->trustProxies(headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO);
 
         $middleware->validateCsrfTokens(except: [
             'payment/pushback',
