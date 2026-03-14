9d0a1d3f89361943d94c1eaed95b91811ad4e178
hourt <bunhourt79@gmail.com>
2026-03-14 11:45:17 +0700

    feat: Add default Laravel database and Redis configuration file.

--- config/database.php
+++ config/database.php
@@ -96,6 +96,9 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'laravel',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'options' => [
+                PDO::ATTR_EMULATE_PREPARES => true,
+            ],
         ],
 
         'sqlsrv' => [
