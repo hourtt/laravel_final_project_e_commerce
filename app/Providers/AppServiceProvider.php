c969c5f0bb015728590a18a1495c63199f94683b
hourt <bunhourt79@gmail.com>
2026-03-20 10:00:39 +0700

    [ADD] Use the new pagination ui design

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use App\Services\PayWayService;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
-
+use \Illuminate\Pagination\Paginator;
 class AppServiceProvider extends ServiceProvider
 {
     // Register any application services.
@@ -20,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
     // Bootstrap any application services.
     public function boot(): void
     {
+        //* Use custom pagination view
+        Paginator::defaultView('vendor.pagination.custom');
+
         //* Setting URL to https when deploying on vercel
         if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
             URL::forceHttps();
