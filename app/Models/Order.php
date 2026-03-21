49af36c18d3d57bccc827666803e568482b8f114
hourt <bunhourt79@gmail.com>
2026-03-21 13:38:04 +0700

    [ADD] Added new fillable column inside Order Model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,7 +8,21 @@ class Order extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id', 'total_price', 'status', 'voucher_code', 'voucher_discount'];
+    protected $fillable = [
+        'user_id',
+        'address_id',
+        'total_price',
+        'status',
+        'voucher_code',
+        'voucher_discount',
+        'shipping_full_name',
+        'shipping_phone_number',
+        'shipping_street_address',
+        'shipping_city',
+        'shipping_state',
+        'shipping_postal_code',
+        'shipping_country',
+    ];
 
     // An order belong to one user
     public function user()
@@ -20,6 +34,14 @@ class Order extends Model
     public function items()
     {
         return $this->hasMany(OrderItem::class);
+    }
+
+    /**
+     * Get the address associated with the order.
+     */
+    public function shippingAddress()
+    {
+        return $this->belongsTo(Address::class, 'address_id');
     }
 }
 
